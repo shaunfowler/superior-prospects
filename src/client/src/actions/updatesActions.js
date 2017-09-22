3050a1cb8983986fde4785956ab220f0ba12f248
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-09-21 22:03:12 -0400

    action and reducer for

---
+++ src/client/src/actions/updatesActions.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import {
+    GET_UPDATES_REQUEST,
+    GET_UPDATES_SUCCESS,
+    GET_UPDATES_FAILURE
+} from './actionTypes';
+
+function createGetUpdatesRequest() {
+    return { type: GET_UPDATES_REQUEST };
+}
+
+function createGetUpdatesSuccess(updates) {
+    return { type: GET_UPDATES_SUCCESS, updates };
+}
+
+function createGetUpdatesFailure(error) {
+    return { type: GET_UPDATES_FAILURE, error };
+}
+
+export function getUpdates() {
+    console.log('sdfds');
+    return dispatch => {
+        dispatch(createGetUpdatesRequest());
+        return axios
+            .get('http://localhost:4000/api/updates')
+            .then(response => {
+                dispatch(createGetUpdatesSuccess(response.data));
+            })
+            .catch(response => {
+                dispatch(createGetUpdatesFailure(response));
+            });
+    };
+}
