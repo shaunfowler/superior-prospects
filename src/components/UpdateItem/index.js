2fe909ca757362d9edff6b775130816a5935c5fe
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-10-06 11:48:58 -0400

    index js for component

---
+++ src/components/UpdateItem/index.js
@@ -0,0 +1,4 @@
+import UpdateItem from "./UpdateItem";
+import "./UpdateItem.less";
+
+export default UpdateItem;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getUpdates } from "../../actions/updatesActions";
-import UpdateItem from "../../components/UpdateItem/UpdateItem";
+import UpdateItem from "../../components/UpdateItem";
 
 class Home extends Component {
   componentWillMount() {
