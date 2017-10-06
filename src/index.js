fa335dfc96139e8ffca1b7a6031bb32efe8d3af9
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-10-06 10:23:21 -0400

    basic component and container structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./containers/App/App";
 
-import { getUpdates } from './actions/updatesActions';
+import { getUpdates } from "./actions/updatesActions";
 
 const store = configureStore();
 
@@ -16,7 +16,7 @@ render(
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 store.dispatch(getUpdates());
