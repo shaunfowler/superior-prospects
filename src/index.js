2d1eccbd541f389d2a9f553221af2736d17963b2
Shaun Fowler <shaunfowler@users.noreply.github.com>
2017-10-09 16:46:55 -0400

    move stuff around (#9)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import configureStore from "./store/configureStore";
+import configureStore from "./redux/store/configureStore";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./containers/App/App";
+import App from "./containers/App";
 
 const store = configureStore();
 
