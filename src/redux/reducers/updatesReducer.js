7593224c863e8555bb38f76bdc5517bea4213cf8
Shaun Fowler <shaun.fowler@qlik.com>
2018-01-08 10:42:29 -0500

    use async utils for baseReducer and updatesReducer

--- src/redux/reducers/updatesReducer.js
+++ src/redux/reducers/updatesReducer.js
@@ -4,12 +4,5 @@ import generateReducer from "./baseReducer";
 import { UPDATE_ACTION_NAMES } from "../actions/updatesActions";
 
 export default function updatesReducer(state, action) {
-  console.log(action);
-  return generateReducer(state, action, {
-    request: UPDATE_ACTION_NAMES.request,
-    failure: UPDATE_ACTION_NAMES.failure,
-    querySuccess: UPDATE_ACTION_NAMES.querySuccess,
-    addSuccess: UPDATE_ACTION_NAMES.addSuccess,
-    deleteSuccess: UPDATE_ACTION_NAMES.deleteSuccess
-  });
+  return generateReducer(state, action, UPDATE_ACTION_NAMES);
 }
