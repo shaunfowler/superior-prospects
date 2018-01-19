cad4aebea9dc837d1734df11d8000fd7fdbdc0c1
Shaun Fowler <shaun.fowler@qlik.com>
2018-01-19 17:04:53 -0600

    eject 1

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,122 @@
+const autoprefixer = require("autoprefixer");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+const eslintFormatter = require("react-dev-utils/eslintFormatter");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+const paths = require("./paths");
+
+const publicPath = "/";
+
+module.exports = {
+  devtool: "cheap-module-source-map",
+  entry: [
+    require.resolve("react-dev-utils/webpackHotDevClient"),
+    paths.appIndexJs
+  ],
+  output: {
+    filename: "static/js/bundle.js",
+    chunkFilename: "static/js/[name].chunk.js",
+    publicPath
+  },
+  resolve: {
+    modules: ["node_modules", paths.appNodeModules, paths.appSrc],
+    extensions: [".js", ".json", ".jsx"],
+    plugins: [new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])]
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: "pre",
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve("eslint")
+            },
+            loader: require.resolve("eslint-loader")
+          }
+        ],
+        include: paths.appSrc
+      },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]"
+            }
+          },
+          {
+            test: /\.(js|jsx)$/,
+            include: paths.appSrc,
+            loader: require.resolve("babel-loader"),
+            options: {
+              cacheDirectory: true
+            }
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              require.resolve("style-loader"),
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  importLoaders: 1
+                }
+              },
+              {
+                loader: require.resolve("postcss-loader"),
+                options: {
+                  ident: "postcss",
+                  plugins: () => [autoprefixer()]
+                }
+              }
+              // require.resolve("sass-loader")
+            ]
+          },
+          {
+            loader: require.resolve("file-loader"),
+            exclude: [/\.js$/, /\.html$/, /\.json$/],
+            options: {
+              name: "static/media/[name].[hash:8].[ext]"
+            }
+          }
+          // Make sure to add the new loader(s) before the "file" loader.
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new InterpolateHtmlPlugin({ PUBLIC_URL: process.env.PUBLIC_URL }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development"),
+        GA_ENABLED: JSON.stringify(true),
+        GA_TRACKING_ID: JSON.stringify("UA-XXXXXXXX-X")
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CaseSensitivePathsPlugin()
+  ],
+  node: {
+    dgram: "empty",
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
+  },
+  performance: {
+    hints: false
+  }
+};
