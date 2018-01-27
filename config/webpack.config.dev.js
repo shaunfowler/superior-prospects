8bafe92a9d5f758decbb5fd8704a18d3a4f14141
Shaun Fowler <shaun.fowler@qlik.com>
2018-01-27 13:05:47 -0500

    dev webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,3 +1,7 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
+
 const autoprefixer = require("autoprefixer");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
