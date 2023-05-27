37c0bedc0d18573ee30931d9c6d253c6645cd569
Shaun Fowler <shaunfowler@users.noreply.github.com>
2023-05-27 14:56:43 -0700

    upgrade to ga4

--- packages/superior-prospects-client/src/containers/Home/Home.js
+++ packages/superior-prospects-client/src/containers/Home/Home.js
@@ -12,12 +12,12 @@ class Home extends Component {
     return (
       <div className="homeView container">
         <Grid container spacing={8}>
-          <Grid item xs={12} sm={12} md={4} lg={4} xl={3}>
+          <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
             <Paper className="paper" elevation={1}>
               <Updates />
             </Paper>
           </Grid>
-          <Grid item xs={12} sm={12} md={8} lg={8} xl={9}>
+          <Grid item xs={12} sm={12} md={9} lg={9} xl={9}>
             <Paper className="paper" elevation={1}>
               <Properties />
             </Paper>
--- packages/superior-prospects-client/src/index.js
+++ packages/superior-prospects-client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import { blue, orange } from "@material-ui/core/colors";
 import store, { history } from "./redux/store/configureStore";
